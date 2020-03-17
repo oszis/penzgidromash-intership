769091411ca2244e80e25fdb4c0a2c569bbf25e4
Anastasia Vyaltseva <anastasiavyalt@gmail.com>
2020-03-18 00:02:21 +0700

    Index page: Catalog section (without adaptivity)

---
+++ src/components/catalog-item/test.js
@@ -0,0 +1,14 @@
+const { describe, it } = require('mocha');
+const { assert } = require('chai');
+
+import CatalogItem from './catalog-item';
+
+describe('Компонент - "catalog-item"', () => {
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {});
+    });
+
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {});
+    });
+});
---
+++ src/components/description-block/test.js
@@ -0,0 +1,14 @@
+const { describe, it } = require('mocha');
+const { assert } = require('chai');
+
+import DescriptionBlock from './description-block';
+
+describe('Компонент - "description-block"', () => {
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {});
+    });
+
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {});
+    });
+});
