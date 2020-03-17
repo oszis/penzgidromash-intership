0ea14f5c7a8fa0a0c760874232c6259ee3491f64
A.Seledtsov <49916559+VitoBravis@users.noreply.github.com>
2020-03-17 19:12:58 +0700

    interactive menu and header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,43 @@
 import Component from '../../common/js/component';
 
 class Header extends Component {
-  constructor(nRoot) {
-    super(nRoot, 'header');
-  }
+    constructor(nRoot) {
+        super(nRoot, 'header');
 
-  destroy() {
+        // const headerNav = document.querySelector('.header__nav-container');
+        // const windowHeight = document.documentElement.clientHeight;
+        const body = document.querySelector('body');
+        const barbaContainer = document.querySelector('.barba-container');
+        const header = document.querySelector('.header');
+        const sandwichBtn = document.querySelector('.sandwich');
 
-  }
+        if (barbaContainer.getAttribute('data-namespace') === 'index') {
+            header.classList.add('header--big-logo');
+        }
+
+        sandwichBtn.addEventListener('click', () => {
+            header.classList.toggle('side-menu--open');
+            body.classList.toggle('no-scroll');
+        });
+
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset <= 0 && !header.classList.contains('header--top')) {
+                header.classList.toggle('header--top');
+                if (barbaContainer.getAttribute('data-namespace') === 'index' && !header.classList.contains('header--big-logo')) {
+                    header.classList.toggle('header--big-logo');
+                }
+            } else if (window.pageYOffset > 0 && header.classList.contains('header--top')) {
+                header.classList.toggle('header--top');
+                if (barbaContainer.getAttribute('data-namespace') === 'index' && header.classList.contains('header--big-logo')) {
+                    header.classList.toggle('header--big-logo');
+                }
+            }
+        });
+    }
+
+    destroy() {
+
+    }
 }
 
 export default Header;
