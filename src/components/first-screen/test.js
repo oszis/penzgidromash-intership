685203f45cc542181fd62e185ec4df851d105b2d
A.Seledtsov <49916559+VitoBravis@users.noreply.github.com>
2020-03-18 16:26:42 +0700

    Fix
    - Fixed first-screen

---
+++ src/components/first-screen/test.js
@@ -0,0 +1,13 @@
+import FirstScreen from './first-screen';
+
+const { describe, it } = require('mocha');
+const { assert } = require('chai');
+
+describe('Компонент - "first-screen"', () => {
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {});
+    });
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {});
+    });
+});
