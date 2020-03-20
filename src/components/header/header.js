e316903b005012fd80a369d1f824aeefb89d7a83
A.Seledtsov <49916559+VitoBravis@users.noreply.github.com>
2020-03-20 17:27:24 +0700

    Added contrast
    - Added contrast effect on scroll to end of index page
    - Fixed svg files
    - Improved BAM for "header"
    - Replaced ".side-menu--open" to ".sanwich-open"

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,26 +5,26 @@ class Header extends Component {
         super(nRoot, 'header');
 
 
+        /* === TODO поиск компонентов через nRoot === */
         let windowHeight = document.documentElement.clientHeight;
         const body = document.querySelector('body');
         const barbaContainer = document.querySelector('.barba-container');
         const header = document.querySelector('.header');
-        const sandwichBtn = document.querySelector('.sandwich');
-        const arrowBtn = document.querySelector('.menu__arrow');
+        const sandwichBtn = document.querySelector('.header__sandwich');
+        const arrowBtn = document.querySelector('.header__arrow');
 
         /* === Входные значения шапки для страницы 'index' === */
-        /* === Может это и не надо, я хз === */
+        /* === TODO должно вызываться при загрузке страницы в barba === */
 
         if (barbaContainer.getAttribute('data-namespace') === 'index') {
-            header.classList.add('header--big-logo');
-            header.classList.add('header--contrast');
+            header.classList.add('big-logo');
+            header.classList.add('contrast-logo');
         }
 
         /* === Клик по кнопке-сендвичу === */
 
         sandwichBtn.addEventListener('click', () => {
-            header.classList.toggle('side-menu--open');
-            body.classList.toggle('no-scroll');
+            body.classList.toggle('sandwich-open');
         });
 
         /* === Клик по кнопке скролла === */
@@ -48,36 +48,40 @@ class Header extends Component {
         window.addEventListener('scroll', () => {
             windowHeight = document.documentElement.clientHeight;
             /* === Появление навигации и изменение логотипа в начале документа === */
-            if (window.pageYOffset <= 0 && !header.classList.contains('header--top')) {
-                header.classList.toggle('header--top');
-                if (barbaContainer.getAttribute('data-namespace') === 'index' && !header.classList.contains('header--big-logo')) {
-                    header.classList.toggle('header--big-logo');
+            if (window.pageYOffset <= 0 && !header.classList.contains('header-top')) {
+                header.classList.toggle('header-top');
+                if (barbaContainer.getAttribute('data-namespace') === 'index' && !header.classList.contains('big-logo')) {
+                    header.classList.toggle('big-logo');
                 }
-            } else if (window.pageYOffset > 0 && header.classList.contains('header--top')) {
-                header.classList.toggle('header--top');
-                if (barbaContainer.getAttribute('data-namespace') === 'index' && header.classList.contains('header--big-logo')) {
-                    header.classList.toggle('header--big-logo');
+            } else if (window.pageYOffset > 0 && header.classList.contains('header-top')) {
+                header.classList.toggle('header-top');
+                if (barbaContainer.getAttribute('data-namespace') === 'index' && header.classList.contains('big-logo')) {
+                    header.classList.toggle('big-logo');
                 }
             }
 
             /* === Появление кнопки скролла только на первом и последнем 'экране' документа === */
-            if (window.pageYOffset < windowHeight || window.pageYOffset >= document.body.scrollHeight - windowHeight * 1.2) {
-                if (window.pageYOffset < windowHeight) {
+            if (window.pageYOffset < windowHeight - 1 || window.pageYOffset >= document.body.scrollHeight - windowHeight * 1.) {
+                if (window.pageYOffset < windowHeight - 1) {
                     header.classList.remove('arrow-up');
                     header.classList.add('arrow-down');
                 }
-                if (window.pageYOffset >= document.body.scrollHeight - windowHeight * 1.2) {
+                if (window.pageYOffset >= document.body.scrollHeight - windowHeight * 1.1) {
                     header.classList.add('arrow-up');
                     header.classList.remove('arrow-down');
+                    if (barbaContainer.getAttribute('data-namespace') === 'index') {
+                        body.classList.add('contrast');
+                    }
                 }
                 /* === Изменение логотипа, чтобы он не сливался с фоном на первом и последнем 'экране' документа === */
                 if (barbaContainer.getAttribute('data-namespace') === 'index') {
-                    header.classList.add('header--contrast');
+                    header.classList.add('contrast-logo');
                 }
             } else {
                 header.classList.remove('arrow-up');
                 header.classList.remove('arrow-down');
-                header.classList.remove('header--contrast');
+                header.classList.remove('contrast-logo');
+                body.classList.remove('contrast');
             }
         });
     }
