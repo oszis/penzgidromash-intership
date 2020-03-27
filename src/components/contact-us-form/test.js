efb8470942e8672c7ea7faf6c7829bedaab14ee4
Артемий Лысиков <artemy0909@users.noreply.github.com>
2020-03-28 00:58:53 +0700

    form in-progress

---
+++ src/components/contact-us-form/test.js
@@ -0,0 +1,16 @@
+const {describe, it} = require('mocha');
+const {assert} = require('chai');
+
+import ContactUsForm from './contact-us-form';
+
+describe('Компонент - "contact-us-form"', () => {
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {
+        });
+    });
+
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {
+        });
+    });
+});
---
+++ src/components/form-input/test.js
@@ -0,0 +1,16 @@
+const {describe, it} = require('mocha');
+const {assert} = require('chai');
+
+import FormInput from './form-input';
+
+describe('Компонент - "form-input"', () => {
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {
+        });
+    });
+
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {
+        });
+    });
+});
