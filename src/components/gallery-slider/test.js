ce2ba4ae36269afef6ce1cbcf1116880179c2cc3
A.Seledtsov <49916559+VitoBravis@users.noreply.github.com>
2020-03-27 14:00:43 +0700

    Typo fix
    - Fixed typo in component.json

---
+++ src/components/gallery-slider/test.js
@@ -0,0 +1,14 @@
+const { describe, it } = require('mocha');
+const { assert } = require('chai');
+
+import GallerySlider from './gallery-slider';
+
+describe('Компонент - "gallery-slider"', () => {
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {});
+    });
+
+    describe('подзаголовок подруппы тестов', () => {
+        it('описание проверки', () => {});
+    });
+});
